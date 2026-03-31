eca42ce9b5c9b22949359e48c890767b0a0994ec
Ryan Keairns <contactryank@gmail.com>
2026-03-31 14:01:22 -0700

    Save progress

--- src/core/packages/chrome/navigation/src/components/side_nav/side_panel.tsx
+++ src/core/packages/chrome/navigation/src/components/side_nav/side_panel.tsx
@@ -35,6 +35,7 @@ const getSidePanelWrapperStyles = (euiThemeContext: UseEuiTheme) => css`
   display: flex;
   flex-direction: column;
   width: ${SIDE_PANEL_WIDTH - SIDE_PANEL_CONTENT_GAP}px;
+  margin-top: ${euiThemeContext.euiTheme.size.s};
   margin-bottom: ${layoutVar('application.marginBottom', '0px')};
   background-color: ${euiThemeContext.euiTheme.colors.backgroundBasePlain};
   border-radius: ${euiThemeContext.euiTheme.border.radius.medium};
